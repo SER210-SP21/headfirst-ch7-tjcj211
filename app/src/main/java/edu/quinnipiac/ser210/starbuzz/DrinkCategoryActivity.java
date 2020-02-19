85c80702cdb033e5791f87c118eb31363904383b
tjcj211 <timcarta@hotmail.com>
2020-02-19 17:38:17 -0500

    Finished CH7Demo

---
+++ app/src/main/java/edu/quinnipiac/ser210/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,41 @@
+package edu.quinnipiac.ser210.starbuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
+
+        //create the listener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                            intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                            startActivity(intent);
+                        }
+                    }
+                };
+
+        //Assign the listener to the list view
+        listDrinks.setOnItemClickListener(itemClickListener);
+    }
+}
